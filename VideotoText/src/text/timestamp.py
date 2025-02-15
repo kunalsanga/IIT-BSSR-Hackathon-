c5f19a0ac14a3472803f4218fffd345205a04b1a
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-15 19:27:18 +0530

    Delete VideotoText directory

--- VideotoText/src/text/timestamp.py
+++
@@ -1,8 +0,0 @@
-def extract_text_by_timestamps(transcript, start_time, end_time):
-    # The transcript is just text, so we don't need to slice it by time
-    # Instead, we'll just return the full text for now
-    return transcript
-
-def convert_to_seconds(timestamp):
-    hours, minutes, seconds = map(int, timestamp.split(':'))
-    return hours * 3600 + minutes * 60 + seconds 
